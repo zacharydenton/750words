ee4b2300dc2201b41f0c72de0f75b2ec6419343c
Zach Denton <zacharydenton@gmail.com>
2013-03-21 13:51:51 -0400

    Refactor to support Python 2 or 3; remove analysis.

--- setup.py
+++ setup.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python
-
 from distutils.core import setup
-import sevenfifty
 
-setup (
-    name = "sevenfifty",
-    description = "Write 750 words daily on the command line.",
-
-    author = sevenfifty.__author__,
-    author_email = sevenfifty.__author_email__,
-    version = sevenfifty.__version__,
-    url = sevenfifty.__url__,
-    long_description = sevenfifty.__longdescr__,
-    classifiers = sevenfifty.__classifiers__,
-    packages = ['sevenfifty',
-               ],
-    scripts = ['750words'],
-    requires = ['nltk']
+setup(
+    name="750words",
+    description="Write 750 words daily on the command line.",
+    author="Zach Denton",
+    author_email="z@chdenton.com",
+    version="0.3",
+    long_description=open('README.md').read(),
+    scripts=['750words']
 )
